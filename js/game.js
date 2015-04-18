4e3f7ec7f17c1e4e9c13baf2a010702957213f80
Mathias Olsson <hypp74@gmail.com>
2015-04-18 10:12:20 +0200

    A moving road

--- js/game.js
+++ js/game.js
@@ -1,135 +1,61 @@
 
 var Phaser = Phaser;
 
+var game_width = 800;
+var game_height = 600;
+
 var game;
-var platforms;
 var player;
 var cursors;
-var stars;
+
 var score = 0;
 var scoreText;
+var bmd;
+var area;
+var dropTime = 0;
 
 function preload() {
     game.load.image('road_light', 'assets/road_light.png');
     game.load.image('road_dark', 'assets/road_dark.png');
-    game.load.image('ground', 'assets/platform.png');
-    game.load.image('star', 'assets/star.png');
-    game.load.spritesheet('dude', 'assets/dude.png', 32, 48);
-}
-
-function collectStar (player, star) {
-
-    // Removes the star from the screen
-    star.kill();
-
-    //  Add and update the score
-    score += 10;
-    scoreText.text = 'Score: ' + score;
 }
 
 function create() {
-    //  We're going to be using physics, so enable the Arcade Physics system
-    game.physics.startSystem(Phaser.Physics.ARCADE);
-
-    //  A simple background for our game
-    game.add.sprite(0, 0, 'road_light');
-
-    //  The platforms group contains the ground and the 2 ledges we can jump on
-    platforms = game.add.group();
-
-    //  We will enable physics for any object that is created in this group
-    platforms.enableBody = true;
-
-    // Here we create the ground.
-    var ground = platforms.create(0, game.world.height - 64, 'ground');
-
-    //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
-    ground.scale.setTo(2, 2);
-
-    //  This stops it from falling away when you jump on it
-    ground.body.immovable = true;
-
-    //  Now let's create two ledges
-    var ledge = platforms.create(400, 400, 'ground');
-
-    ledge.body.immovable = true;
-
-    ledge = platforms.create(-150, 250, 'ground');
-
-    ledge.body.immovable = true;
     
-    // The player and its settings
-    player = game.add.sprite(32, game.world.height - 150, 'dude');
+	game.stage.backgroundColor = '#2d2d2d';
 
-    //  We need to enable physics on the player
-    game.physics.arcade.enable(player);
+	bmd = game.make.bitmapData(game_width, game_height);
 
-    //  Player physics properties. Give the little guy a slight bounce.
-    player.body.bounce.y = 0.2;
-    player.body.gravity.y = 300;
-    player.body.collideWorldBounds = true;
+	bmd.addToWorld();
 
-    //  Our two animations, walking left and right.
-    player.animations.add('left', [0, 1, 2, 3], 10, true);
-    player.animations.add('right', [5, 6, 7, 8], 10, true);
-    
-    cursors = game.input.keyboard.createCursorKeys();
-    
-    stars = game.add.group();
+	area = new Phaser.Rectangle(0, 0, game_width, 1);
 
-    stars.enableBody = true;
-
-    //  Here we'll create 12 of them evenly spaced apart
-    for (var i = 0; i < 12; i++) {
-        //  Create a star inside of the 'stars' group
-        var star = stars.create(i * 70, 0, 'star');
-
-        //  Let gravity do its thing
-        star.body.gravity.y = 6;
-
-        //  This just gives each star a slightly random bounce value
-        star.body.bounce.y = 0.7 + Math.random() * 0.2;
-    }
-
-    scoreText = game.add.text(16, 16, 'score: 0', { fontSize: '32px', fill: '#000' });
 }
 
+var z_pos = 0;
+var z_speed = 256;
+    
 function update() {
-    game.physics.arcade.collide(stars, platforms);
-    game.physics.arcade.overlap(player, stars, collectStar, null, this);
+
+    z_pos = (z_pos + z_speed) % (1024*4);
     
- //  Collide the player and the stars with the platforms
-    game.physics.arcade.collide(player, platforms);
+    var z = z_pos;
+    var z_inc = 1;
+    var z_cur = 0;
     
-//  Reset the players velocity (movement)
-    player.body.velocity.x = 0;
+    for (var y = game_height-1; y >= 0; y--) {
+        area.y = y;
+        z_cur += z_inc;
+        z = (z + z_cur) % (1024*4);
 
-    if (cursors.left.isDown) {
-        //  Move to the left
-        player.body.velocity.x = -150;
-
-        player.animations.play('left');
-    } else if (cursors.right.isDown) {
-        //  Move to the right
-        player.body.velocity.x = 150;
-
-        player.animations.play('right');
-    } else {
-        //  Stand still
-        player.animations.stop();
-
-        player.frame = 4;
+        if (z < (512*4)) {
+            bmd.copyRect('road_dark', area, 0, y);
+        } else {
+            bmd.copyRect('road_light', area, 0, y);
+        }
     }
-
-    //  Allow the player to jump if they are touching the ground.
-    if (cursors.up.isDown && player.body.touching.down) {
-        player.body.velocity.y = -350;
-    }
-    
-
 }
 
-var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
+var game = new Phaser.Game(game_width, game_height, Phaser.AUTO, '', {
         preload: preload,
         create: create,
         update: update
